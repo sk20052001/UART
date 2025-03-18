87475e1ea073365f27d6ea0fbdc76f8406a2abd4
sk20052001 <sanjeev@pdx.edu>
2025-03-17 17:59:57 -0700

    Datawidth specified dynamically in pkg

--- Verification/tb_pkg.sv
+++ Verification/tb_pkg.sv
@@ -1,8 +1,8 @@
 package tb_pkg;
     class randomData #(parameter int DATA_WIDTH = 8);
-        rand bit [DATA_WIDTH:0] data;
+        rand bit [DATA_WIDTH-1:0] data;
 
-        function bit [7:0] randomc();
+        function bit [DATA_WIDTH-1:0] randomc();
             if (!this.randomize()) begin
                 $display("Randomization failed.");
             end
